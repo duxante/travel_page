89f07a29ca02622931631f658c7d5cfd744c37f9
duxante <spomenko.dukic@gmail.com>
2023-10-10 19:08:39 +0200

    adding hamburger

--- src/features/navigation/navigation.component.jsx
+++ src/features/navigation/navigation.component.jsx
@@ -1,23 +1,119 @@
-import "./navigation.style.css";
-import Button from "../../common/button/button.component";
-import travel_logo from "../../assets/travel_logo.png";
+import * as React from 'react';
+import './navigation.style.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import travel_logo from '../../assets/travel_logo.png';
 
-const Navigation = () => {
-    return(
-        <div className="navigationHolder">
-            <div className="navigation">
-                <img src={travel_logo} alt="travel logo" />
-        <div className="contactHolder">
-            <p>Home</p>
-            <p>About</p>
-            <p>App</p>
-            <Button
-                buttonText="Sign Up for Free"
+const pages = ['Home', 'About', 'App'];
+
+const NavigationBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl" sx={{ backgroundColor: '#deebf3' }}>
+        <Toolbar disableGutters>
+          <Box
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+            }}
+          >
+            <img
+              className="navigationBarLogo"
+              src={travel_logo}
+              alt="travel logo"
             />
-        </div>
-            </div>
-        </div>
-    )
-}
+          </Box>
 
-export default Navigation;
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="black"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+            }}
+          >
+            <img
+              className="navigationBarLogo"
+              src={travel_logo}
+              alt="travel logo"
+            />
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 0,
+              marginLeft: 'auto',
+              display: { xs: 'none', md: 'flex' },
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: '#29417c',
+                  fontWeight: 'bold',
+                  display: 'block',
+                }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default NavigationBar;
